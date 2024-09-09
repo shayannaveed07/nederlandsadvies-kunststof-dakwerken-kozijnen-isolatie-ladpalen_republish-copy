f9a37025f169e4ecaedec9a69b254a7056583991
scorpionew28 <shayan_naveed@nextpak.org>
2024-09-09 14:19:00 +0500

    commit all files

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <NavBar />
+  </div>
+</template>
+<script>
+import NavBar from "./views/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
+<style></style>
